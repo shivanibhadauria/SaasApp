e174a160cf69273d3ca887f04268d10c64d428e3
Shivani Bhadauria <shivanibhadauria921997@gmal.com>
2024-12-01 14:10:46 +0530

    Clerk authentication added

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { IBM_Plex_Sans } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import { ClerkProvider , SignedOut, SignedIn, SignInButton, UserButton } from "@clerk/nextjs";
 
 const IBMPlex = IBM_Plex_Sans({
   subsets: ["latin"],
@@ -20,12 +21,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+    <ClerkProvider>
     <html lang="en">
       <body
         className={cn("font-IBMPlex antialiased" , IBMPlex.variable)}
       >
+        
         {children}
       </body>
-    </html>
+    </html></ClerkProvider>
   );
 }
