beb664f0014deb45ba5c49f0d126a43ff63be34a
Shivani Bhadauria <shivanibhadauria921997@gmal.com>
2024-12-05 11:06:49 +0530

    error fixed

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,10 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
-const middleware = clerkMiddleware({
-    publishableKey: process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
-  });
-export default middleware
+const isProtectedRoute = createRouteMatcher(['/', '/api/webhooks/clerk', '/api/webhooks/stripe'])
 
-console.log("Clerk Publishable Key:", process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY);
+export default clerkMiddleware(async (auth, req) => {
+  if (!isProtectedRoute(req)) await auth.protect()
+})
 
 export const config = {
   matcher: [
@@ -14,4 +13,4 @@ export const config = {
     // Always run for API routes
     '/(api|trpc)(.*)',
   ],
-};
+}
